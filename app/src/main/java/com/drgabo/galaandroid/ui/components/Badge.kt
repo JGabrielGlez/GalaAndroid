ab1aa4775248dcc4e8797c07587da3d421792325
JGabrielGlez <jesusgabrielge@hotmail.com>
2026-06-19 15:13:26 -0700

    badge estado completado

---
+++ app/src/main/java/com/drgabo/galaandroid/ui/components/Badge.kt
@@ -0,0 +1,50 @@
+package com.drgabo.galaandroid.ui.components
+
+import com.drgabo.galaandroid.ui.theme.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.drgabo.galaandroid.ui.theme.GalaAndroidTheme
+
+@Composable
+fun BadgeEstado(
+    modifier: Modifier = Modifier,
+    mensaje: String,
+    colorFondo: Color = Color.Green.copy(alpha = .5f),
+    colorTexto:Color=Color.Green,
+
+) {
+    Surface(
+        color=colorFondo,
+        contentColor = colorTexto,
+        shape = RoundedCornerShape(50)
+    ) {
+        Text(text = mensaje,
+            textAlign = TextAlign.Center,
+            color=colorTexto,
+            modifier= Modifier.padding(horizontal = 8.dp, vertical = 2.dp))
+    }
+}
+
+@Preview(showBackground = true, widthDp = 100)
+@Composable
+fun ShowBadgeEstado(){
+    GalaAndroidTheme {
+        BadgeEstado(mensaje = "Confirmada",
+            colorFondo = EstadoConfirmadaFondo,
+            colorTexto = EstadoConfirmada)
+
+    }
+}
