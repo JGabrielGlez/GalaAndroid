0803fb5caf8e5eb4b6403aca23154033ee01c0a1
JGabrielGlez <jesusgabrielge@hotmail.com>
2026-06-18 22:48:25 -0700

    Colores del navbar

--- app/src/main/java/com/drgabo/galaandroid/MainActivity.kt
+++ app/src/main/java/com/drgabo/galaandroid/MainActivity.kt
@@ -1,4 +1,34 @@
 package com.drgabo.galaandroid
 
-class MainActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity(){
+     override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(
+            savedInstanceState
+        )
+        setContent{
+            //aquí ya se pueden empezar a llamar elementos composables para construir la app
+            MaterialTheme(){
+                Greetin("Soy Gabriel ")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greetin(name:String){
+        Text(text = "oHola mundo $name")
+}
+
+@Preview
+@Composable
+fun PreviewRange(){
+    Greetin("Gabriel")
 }
---
+++ app/src/main/java/com/drgabo/galaandroid/ui/components/NavBar.kt
@@ -0,0 +1,184 @@
+package com.drgabo.galaandroid.ui.components
+
+import androidx.compose.foundation.border
+import  com.drgabo.galaandroid.ui.icons.home
+import  com.drgabo.galaandroid.ui.icons.menu
+import  com.drgabo.galaandroid.ui.icons.account_circle
+import  com.drgabo.galaandroid.ui.icons.content_cut
+import  com.drgabo.galaandroid.ui.icons.view_agenda
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.drgabo.galaandroid.ui.theme.AcentoPrincipal
+import com.drgabo.galaandroid.ui.theme.AcentoSuave
+import com.drgabo.galaandroid.ui.theme.TextoSecundario
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Home Screen")
+    }
+}
+
+
+@Composable
+fun AgendaScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Agenda Screen")
+    }
+}
+
+
+@Composable
+fun ServiceScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+           ,
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Service Screen")
+    }
+}
+
+
+@Composable
+fun ClientScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Client Screen")
+    }
+}
+
+
+@Composable
+fun MoreScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("More Screen")
+    }
+}
+
+enum class Destination(
+    val route: String,
+    val label: String,
+    val icon: ImageVector,
+    val contentDescription: String
+) {
+    HOME("home", "Inicio", home, "Inicio"),
+    AGENDA("agenda", "Agenda", view_agenda, "Agenda"),
+
+    SERVICES("services", "Servicios", content_cut, "Servicios"),
+    CLIENTS(
+        route = "clients",
+        label = "Clientes",
+        icon = account_circle,
+        contentDescription = "Clientes"
+    ),
+    MORE(route = "more", label = "Más", icon = menu, contentDescription = "Menú")
+}
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController,
+    startDestination: Destination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController,
+        startDestination = startDestination.route,
+
+    ) {
+        Destination.entries.forEach { destination ->
+            composable(destination.route) {
+                when (destination) {
+                    Destination.SERVICES -> ServiceScreen()
+                    Destination.AGENDA->AgendaScreen()
+                    Destination.HOME->HomeScreen()
+                    Destination.CLIENTS -> ClientScreen()
+                    Destination.MORE->MoreScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun NavBar(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    val startDestination = Destination.HOME
+    var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+                Destination.entries.forEachIndexed { index, destination ->
+                    NavigationBarItem(
+                        selected = selectedDestination == index,
+                        onClick = {
+                            navController.navigate(route = destination.route)
+                            selectedDestination = index
+                        },
+                        icon = {
+                            Icon(
+
+                                destination.icon,
+                                contentDescription = destination.contentDescription
+                            )
+                        },
+                        label = { Text(destination.label) },
+                        colors =
+                            NavigationBarItemDefaults.colors(
+                               selectedIconColor = AcentoPrincipal,
+                                unselectedIconColor = Color.Gray,
+                                selectedTextColor = AcentoPrincipal,
+                                unselectedTextColor = Color.Gray,
+                                indicatorColor = AcentoSuave.copy(alpha = .45f)
+                            )
+
+                    )
+                }
+            }
+        }
+    ) { contentPadding ->
+        AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding))
+    }
+}
