55a817dee3a29a9288494a3b1388b131158de59e
JGabrielGlez <jesusgabrielge@hotmail.com>
2026-06-18 19:02:58 -0700

    FirstCommit

---
+++ app/src/main/java/com/drgabo/galaandroid/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.drgabo.galaandroid
+
+class MainActivity {
+}
---
+++ app/src/main/java/com/drgabo/galaandroid/ui/theme/Type.kt
@@ -0,0 +1,22 @@
+package com.drgabo.galaandroid.ui.theme
+
+import com.drgabo.galaandroid.R
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+
+)
+
+val MonstserratFamily = FontFamily(
+    Font(R.font.montserrat_black, FontWeight.Black),
+    Font(R.font.montserrat_bold, FontWeight.Bold),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_semi_bold, FontWeight.SemiBold)
+)
