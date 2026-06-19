69ca4f57edf0a2ef3115cd15c58eb5382083e7f0
JGabrielGlez <jesusgabrielge@hotmail.com>
2026-06-19 13:32:26 -0700

    Summary card termianda

--- app/src/main/java/com/drgabo/galaandroid/ui/theme/Type.kt
+++ app/src/main/java/com/drgabo/galaandroid/ui/theme/Type.kt
@@ -8,10 +8,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-
-)
 
 val MonstserratFamily = FontFamily(
     Font(R.font.montserrat_black, FontWeight.Black),
@@ -20,3 +16,33 @@ val MonstserratFamily = FontFamily(
     Font(R.font.montserrat_regular, FontWeight.Normal),
     Font(R.font.montserrat_semi_bold, FontWeight.SemiBold)
 )
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+
+    displayLarge = Typography().displayLarge.copy(fontFamily = MonstserratFamily),
+    displayMedium = Typography().displayMedium.copy(fontFamily = MonstserratFamily),
+    displaySmall = Typography().displaySmall.copy(fontFamily = MonstserratFamily),
+
+
+    headlineLarge = Typography().headlineLarge.copy(fontFamily = MonstserratFamily),
+    headlineMedium = Typography().headlineMedium.copy(fontFamily = MonstserratFamily),
+    headlineSmall = Typography().headlineSmall.copy(fontFamily = MonstserratFamily),
+
+
+    titleLarge = Typography().titleLarge.copy(fontFamily = MonstserratFamily),
+    titleMedium = Typography().titleMedium.copy(fontFamily = MonstserratFamily),
+    titleSmall = Typography().titleSmall.copy(fontFamily = MonstserratFamily),
+
+
+    bodyLarge = Typography().bodyLarge.copy(fontFamily = MonstserratFamily),
+    bodyMedium = Typography().bodyMedium.copy(fontFamily = MonstserratFamily),
+    bodySmall = Typography().bodySmall.copy(fontFamily = MonstserratFamily),
+
+
+    labelLarge = Typography().labelLarge.copy(fontFamily = MonstserratFamily),
+    labelMedium = Typography().labelMedium.copy(fontFamily = MonstserratFamily),
+    labelSmall = Typography().labelSmall.copy(fontFamily = MonstserratFamily),
+
+
+    )
